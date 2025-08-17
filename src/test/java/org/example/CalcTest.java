e6a22a934db2765a974708c4a2db7a06b6b7609d
Nan LI <nan.li@gamil.com>
2025-08-17 18:44:03 +1200

    Add Failing Test

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,10 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
+    // add new test make run red
+    @Test
+    void testSubtraction() {
+        assertEquals(2, c.subtract(4, 2));
+    }
 
 }
